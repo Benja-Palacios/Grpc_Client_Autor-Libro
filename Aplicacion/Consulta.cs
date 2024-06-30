0c277eb978eb7a7db9ec5da8cce54f65ca19372a
Benjamin <benjaminmpalacios@gmail.com>
2024-06-30 17:56:36 -0600

    Ajustar codigo con Polly

--- Aplicacion/Consulta.cs
+++ Aplicacion/Consulta.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using Grpc_AutorImagen;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Polly;
 
 namespace Api.Microservice.Autor.Aplicacion
 {
@@ -11,21 +12,23 @@ namespace Api.Microservice.Autor.Aplicacion
     {
         public class ListaAutor : IRequest<List<AutorDto>>
         {
-
         }
-        //recibe ListaAutor y regresa una lista de AutorLibro
+
         public class Manejador : IRequestHandler<ListaAutor, List<AutorDto>>
         {
             private readonly ContextoAutor _context;
             private readonly IMapper _mapper;
             private readonly AutorImagenService.AutorImagenServiceClient _grpcClient;
+            private readonly IAsyncPolicy<ImagenResponse> _fallbackPolicy;
 
-            public Manejador(ContextoAutor context, IMapper mapper, AutorImagenService.AutorImagenServiceClient grpcClient)
+            public Manejador(ContextoAutor context, IMapper mapper, AutorImagenService.AutorImagenServiceClient grpcClient, IAsyncPolicy<ImagenResponse> fallbackPolicy)
             {
-                this._context = context;
-                this._mapper = mapper;
-                this._grpcClient = grpcClient;
+                _context = context;
+                _mapper = mapper;
+                _grpcClient = grpcClient;
+                _fallbackPolicy = fallbackPolicy;
             }
+
             public async Task<List<AutorDto>> Handle(ListaAutor request, CancellationToken cancellationToken)
             {
                 var autores = await _context.AutorLibros.ToListAsync(cancellationToken);
@@ -38,15 +41,28 @@ namespace Api.Microservice.Autor.Aplicacion
                         IdAutorLibro = autor.AutorLibroGuid
                     };
 
-                    var grpcResponse = await _grpcClient.ObtenerImagenAsync(grpcRequest);
-
-                    if (grpcResponse != null && grpcResponse.Contenido != null)
+                    try
                     {
-                        autor.Imagenes = grpcResponse.Contenido.ToByteArray();
+                        var grpcResponse = await _fallbackPolicy.ExecuteAsync(async () =>
+                        {
+                            return await _grpcClient.ObtenerImagenAsync(grpcRequest);
+                        });
+
+                        if (grpcResponse != null && grpcResponse.Contenido != null)
+                        {
+                            autor.Imagenes = grpcResponse.Contenido.ToByteArray();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // Log the error or handle it accordingly
+                        // Console.WriteLine($"Error fetching image for {autor.AutorLibroGuid}: {ex.Message}");
                     }
                 }
                 return autoresDto;
             }
         }
     }
+
+
 }
